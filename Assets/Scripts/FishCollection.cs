0075b3f47bc338139b8c99bd7590add5f0b00fd0
anh0512 <anh21@ru.is>
2022-12-01 15:49:40 +0000

    added gate  sprites, animation and script

--- Assets/Scripts/FishCollection.cs
+++ Assets/Scripts/FishCollection.cs
@@ -6,12 +6,18 @@ using UnityEngine.UI;
 public class FishCollection : MonoBehaviour
 {
     private int fishCollected = 0;
+    bool isOpen = false;
 
     [SerializeField]private Text fishText;
     [SerializeField]private int totalFish = 1;
+    [SerializeField]private GateController gate;
     [SerializeField]private GameObject levelFinish;
     [SerializeField] private AudioSource itemCollectingSound;
 
+
+    void Start()
+    {
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -26,16 +32,11 @@ public class FishCollection : MonoBehaviour
 
     private void Update()
     {
-        if (fishCollected == totalFish)
+        if (fishCollected == totalFish && !isOpen)
         {
-            GameObject[] goArray = GameObject.FindGameObjectsWithTag("BlockBox");
-            if (goArray.Length > 0)
-            {
-                for (int i = 0; i < goArray.Length; i++)
-                {
-                    goArray[i].SetActive(false);
-                }
-            }
+            gate = FindObjectOfType<GateController>();
+            gate.OpenGate();
+            isOpen = true; 
         }
     }
     
