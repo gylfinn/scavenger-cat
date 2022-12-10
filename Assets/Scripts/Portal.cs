921d7535ac21761de2b8487884f6399e019d0157
lorraineros <lorrainert2002@gmail.com>
2022-12-10 00:26:26 +0000

    added sound effect to the portal ...
    when portal is inactive it turns black, enemies also have invulnerability to the portal

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -8,9 +8,14 @@ public class Portal : MonoBehaviour
 
     public bool isPortal2;
     public float distance = 0.2f;
+    [SerializeField]private AudioSource teleportationSound;
+    private SpriteRenderer spriteRend1;
+    private SpriteRenderer spriteRend2;
 
     void Start()
     {
+        spriteRend1 = GameObject.FindGameObjectWithTag("Portal1").GetComponent<SpriteRenderer>();
+        spriteRend2 = GameObject.FindGameObjectWithTag("Portal2").GetComponent<SpriteRenderer>();
         if (isPortal2 == false)
         {
             destination = GameObject.FindGameObjectWithTag("Portal2").GetComponent<Transform>();
@@ -25,6 +30,7 @@ public class Portal : MonoBehaviour
         if (Vector2.Distance(transform.position, other.transform.position) > distance)
         {
             other.transform.position = new Vector2(destination.position.x, destination.position.y);
+            teleportationSound.Play();
             StartCoroutine(Invulnerability());
         }
 
@@ -35,7 +41,13 @@ public class Portal : MonoBehaviour
     private IEnumerator Invulnerability()
     {
         Physics2D.IgnoreLayerCollision(9, 11, true);
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        spriteRend1.color = Color.black;
+        spriteRend2.color = Color.black;
         yield return new WaitForSeconds(2);
+        spriteRend1.color = Color.white;
+        spriteRend2.color = Color.white;
         Physics2D.IgnoreLayerCollision(9, 11, false);
+        Physics2D.IgnoreLayerCollision(10, 11, false);
     }
 }
